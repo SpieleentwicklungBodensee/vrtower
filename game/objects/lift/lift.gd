0ad23686f3922c0e43b86fa2bef66951de7823f3
Benjamin Dums <benjamin.dums@gmail.com>
2026-02-01 00:40:39 +0100

    added new experimental levels and music

--- game/objects/lift/lift.gd
+++ game/objects/lift/lift.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
-@export var distance: float = 20.3
+@export var distance: float = 20.1
 @export var speed: float = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var ab: LiftMover = $AnimatableBody3D as LiftMover
+	ab.distance = distance
+	ab.speed = speed
+	ab.update()
